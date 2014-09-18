05d44f4477189c3243099bae38dd1ab51da50d92
Mr-zeng <zengwenhao0710@gmail.com>
2014-09-18 17:40:46 +0800

    update *

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -75,7 +75,7 @@ TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/class/android_usb/android0/f_mass_stora
 # LCD
 TARGET_BRIGHTNESS_PATH := /sys/devices/platform/leds-mt65xx/leds/lcd-backlight/brightness
 TARGET_MAX_BRIGHTNESS := 255
-TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888″
+TARGET_RECOVERY_PIXEL_FORMAT := "RGB_565″
 
 # NO misc
 # BOARD_HAS_NO_MISC_PARTITION := true
